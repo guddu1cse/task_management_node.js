61c66ea35231c6028a617b43b3e5278b53e0d7d1
Guddu Kumar <guddu.kumar.31.3@mountblue.tech>
2025-03-24 11:11:55 +0530

    creating task protected routes

--- src/routes/taskRoute.js
+++ src/routes/taskRoute.js
@@ -1,15 +1,25 @@
 import express from 'express';
-import authenticateUser from '../middleware/authenticateUser.js';
-import { createTask, filterByPriority, getTasks, removeTask, sortBy, queryTask, updateTask } from '../controllers/taskController.js';
+import authenticateUser from '../middleware/authenticateUser.js'; //importing middleware for authenticating the user
+import { createTask, filterByPriority, getTasks, removeTask, sortBy, queryTask, updateTask } from '../controllers/taskController.js'; //importing controllers
 
+// router
 const router = express.Router();
 
+
+//protected routes
+//setting up routes
 router.post("/", authenticateUser, createTask);
+//getting all the tasks related to logged in user
 router.get("/tasks", authenticateUser, getTasks);
+//getting single task
 router.put("/:id", authenticateUser, updateTask);
+//deleting single task
 router.delete("/:id", authenticateUser, removeTask);
+//sorting task
 router.get("/sortBy/:sortBy/sortOrder/:sortOrder", authenticateUser, sortBy);
+//filtering task
 router.get("/filterBy/:filterBy/filterOn/:filterOn", authenticateUser, filterByPriority);
+//getting task by queryed params
 router.get("/query", authenticateUser, queryTask);
 
 export default router;
