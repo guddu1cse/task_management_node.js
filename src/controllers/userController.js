a2c36aaa26816b8cb43504d83dbd42a6dc30904b
Guddu Kumar <guddu.kumar.31.3@mountblue.tech>
2025-03-24 10:19:29 +0530

    added comments and removing unused methods , console and variables

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,21 +1,24 @@
-import { getTaskById } from "../db/teskQuery.js";
 import { createUser as addUser, getUserById } from "../db/userQuery.js";
 const getUrl = (req, res) => {
     res.send("Hello World! This is a user route. You can access this route by visiting http://localhost:3000/users");
 };
 
+// get all users controller
 const getUsers = async (req, res) => {
     const { id } = req.params;
     const user = await getUserById(id);
     res.status(200).json(user);
 };
 
+
+// get user controller
 const getUser = (req, res) => {
     const { id } = req.params;
     console.log(id);
     res.send(getUserById(id));
 }
 
+// create user controller
 const createUser = (req, res) => {
     res.send(addUser(req.body));
 }
