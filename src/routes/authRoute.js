f719054969a59741e0385b6f37f1d3119b3ce612
Guddu Kumar <guddu.kumar.31.3@mountblue.tech>
2025-03-24 11:09:17 +0530

    added auth router for login/register the user

--- src/routes/authRoute.js
+++ src/routes/authRoute.js
@@ -1,8 +1,10 @@
 import express from 'express';
 import { getProfile, loginUser } from '../controllers/authController.js';
-import authenticateUser from '../middleware/authenticateUser.js';
+import authenticateUser from '../middleware/authenticateUser.js'; //importing middleware for authenticating the user
+// router
 const router = express.Router();
 
+//setting up routes
 router.post('/login', loginUser); //login user endpoint
 router.get('/profile', authenticateUser, getProfile); //find current user endpoint
 
