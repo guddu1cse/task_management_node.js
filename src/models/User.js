6bac335dae82585d10694442507f5aa883df9c5c
Guddu Kumar <guddu.kumar.31.3@mountblue.tech>
2025-03-24 11:07:51 +0530

    user mode/table creating using sequeilize

--- src/models/User.js
+++ src/models/User.js
@@ -2,17 +2,21 @@ import { DataTypes } from "sequelize";
 import sequelize from "../config/database.js";
 import { hashPassword } from "../utils/passwordUtils.js";
 
+//user model
 const User = sequelize.define("User", {
+    //primary key
     id: {
         type: DataTypes.INTEGER,
         primaryKey: true,
         autoIncrement: true,
     },
+    //username as string
     username: {
         type: DataTypes.STRING,
         allowNull: false,
         unique: true,
     },
+    //email as email
     email: {
         type: DataTypes.STRING,
         allowNull: false,
@@ -21,22 +25,29 @@ const User = sequelize.define("User", {
             isEmail: true,
         },
     },
+    //password as string before saving it will hashed it
     password: {
         type: DataTypes.STRING,
         allowNull: false,
     },
+    //timestamps
     createdAt: {
         type: DataTypes.DATE,
         defaultValue: DataTypes.NOW,
     },
+    //timestamps
     updatedAt: {
         type: DataTypes.DATE,
         defaultValue: DataTypes.NOW,
     },
 }, {
+    //hooks
     hooks: {
+        //before creating user hashing the password from row password
         beforeCreate: async (user) => {
+            //checking password is valid
             if (user.password) {
+                //hashing password
                 user.password = await hashPassword(user.password);
             }
         },
