e7d741e97e598401151a8c924e6bf2842e149eab
Guddu Kumar <guddu.kumar.31.3@mountblue.tech>
2025-03-24 10:10:52 +0530

    sequelize configuration

--- src/config/database.js
+++ src/config/database.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import { DB_HOST, DB_NAME, DB_PASSWORD, DB_USER, DB_DIALECT } from "./config.js";
 dotenv.config();
 
+//sequelize configuration
 const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
     host: DB_HOST,
     dialect: DB_DIALECT,
