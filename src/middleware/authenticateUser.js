d4f567a51c0cfe6e22eae37bfe052273b825ae79
Guddu Kumar <guddu.kumar.31.3@mountblue.tech>
2025-03-24 11:02:06 +0530

    added user authenticating while login

--- src/middleware/authenticateUser.js
+++ src/middleware/authenticateUser.js
@@ -4,20 +4,25 @@ import { JWT_SECRET } from "../config/config.js";
 dotenv.config();
 
 
+//middleware for authenticating the while login user
 const authenticateUser = (req, res, next) => {
-    console.log(req.header("Authorization"));
+    // console.log(req.header("Authorization"));
+    //getting token from header
     const token = req.header("Authorization")?.split(" ")[1];
 
+    //if token is not provided then send error
     if (!token) {
         return res.status(401).json({ data: null, message: "Access Denied! No token provided.", error: null });
     }
 
+
     try {
+        //verifying token
         const logedInUser = jwt.verify(token, JWT_SECRET);
-        req.user = logedInUser;
-        console.log(logedInUser);
+        req.user = logedInUser; //storing user in request
         next();
     } catch (error) {
+        //if token is invalid then send error
         res.status(401).json({ data: null, message: "Access Denied! Invalid token.", error: error });
     }
 };
