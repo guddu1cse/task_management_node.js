8e208de1ea5f1681766bfd7ebba02b0e46a7e1b5
Guddu Kumar <guddu.kumar.31.3@mountblue.tech>
2025-03-24 11:15:34 +0530

    creating password utils for hashing the password and comparing the password

--- src/utils/passwordUtils.js
+++ src/utils/passwordUtils.js
@@ -3,22 +3,31 @@ import dotenv from "dotenv";
 import { SALT_ROUNDS } from "../config/config.js";
 dotenv.config();
 
+//hashing password function
 const hashPassword = async (password) => {
     try {
+        //generating salt
         const salt = await bcrypt.genSalt(SALT_ROUNDS);
+        //hashing password
         const hashedPassword = await bcrypt.hash(password, salt);
+        //return hashed password
         return hashedPassword;
     } catch (error) {
+        //throw error
         throw new Error("Error hashing password");
     }
 };
 
+//comparing password hashpassword with plain password which is coming from user while login
 const comparePassword = async (password, hashedPassword) => {
     try {
+        //compare password
         return await bcrypt.compare(password, hashedPassword);
     } catch (error) {
+        //throw error
         throw new Error("Error comparing password");
     }
 };
 
+//exporting functions
 export { hashPassword, comparePassword };
